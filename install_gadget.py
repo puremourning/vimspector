85d6fbe480288c5c7047d343e5701d76e093d694
Ben Jackson <puremourning@gmail.com>
2020-02-08 18:09:13 +0000

    Use YCM python style

--- install_gadget.py
+++ install_gadget.py
@@ -529,7 +529,7 @@ def DownloadFileTo( url,
                     destination,
                     file_name = None,
                     checksum = None,
-                    sslcheck = True):
+                    sslcheck = True ):
   if not file_name:
     file_name = url.split( '/' )[ -1 ]
 
