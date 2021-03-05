3357077e9439095e9eb1d428ccea79523eb1cc70
Ben Jackson <puremourning@gmail.com>
2021-03-05 21:10:08 +0000

    WIP: Data breakpoints (though can't find a server which supports them)

--- support/test/go/hello_world/hello-world.go
+++ support/test/go/hello_world/hello-world.go
@@ -1,6 +1,25 @@
 package main
 import "fmt"
+
+type Toaster struct {
+	Power int
+	Colour string
+}
+
+type Test struct {
+	Test string
+	Toast Toaster
+}
+
 func main() {
   var v = "test"
+	test := Test{
+		Test: "This is\na\ntest",
+		Toast: Toaster{
+			Power: 10,
+			Colour: "green",
+		},
+	}
   fmt.Println("hello world: " + v)
+	fmt.Println("Hi " + test.Test)
 }
