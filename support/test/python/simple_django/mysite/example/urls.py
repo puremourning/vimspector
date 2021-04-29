94e8a6d35b555dbbd1dbbef28e1ea1bf831eb8b2
Ben Jackson <puremourning@gmail.com>
2021-04-29 19:13:10 +0100

    Django test

---
+++ support/test/python/simple_django/mysite/example/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+  path( '', views.index, name='index' )
+]
+
---
+++ support/test/python/simple_django/mysite/example/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+  return HttpResponse( "Hello, world" )
