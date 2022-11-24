5893c2befbf53b6486a8edac5fc64d549516948d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 08:52:27 +0000

    Adding tarfile member sanitization to extractall()

--- python3/vimspector/installer.py
+++ python3/vimspector/installer.py
@@ -753,7 +753,26 @@ def ExtractZipTo( file_path, destination, format ):
   elif format == 'tar':
     try:
       with tarfile.open( file_path ) as f:
-        f.extractall( path = destination )
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(f, path=destination)
     except Exception:
       # There seems to a bug in python's tarfile that means it can't read some
       # windows-generated tar files
