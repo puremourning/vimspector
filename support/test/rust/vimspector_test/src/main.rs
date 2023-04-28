7f5f3c824a114801cf047d2abb90a0a1015b985a
Ben Jackson <puremourning@gmail.com>
2023-04-28 23:00:13 +0100

    Make data breakpoints sort of work
    Works with CodeLLDB mostly for named things. Expressions don't seem to
    be supported by anyone.
    
    Fixed some state issues with the connections being retained across
    restarts in watches. ugh. so messy.
    
    Added access type question.

--- support/test/rust/vimspector_test/src/main.rs
+++ support/test/rust/vimspector_test/src/main.rs
@@ -1,4 +1,18 @@
+struct Point {
+  x: i32,
+  y: i32,
+}
+
 fn main() {
   let s = "World!";
   println!("Hello, {}!", s);
+
+  let mut p = Point{ x: 1, y: 11 };
+
+  p.x = 11;
+  p.y = 11;
+  p.x += 11;
+  p.y += 11;
+
+  println!("Point: {}, {}", p.x, p.y);
 }
