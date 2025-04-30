2ee6dfb0f452bdfd5ecf87d9a75edd02ccd21973
Luka Markušić <markusicluka@gmail.com>
2025-04-30 08:27:39 +0200

    Update delve to 1.24.2

--- python3/vimspector/gadgets.py
+++ python3/vimspector/gadgets.py
@@ -353,7 +353,7 @@ GADGETS = {
                                                              gadget ),
     'all': {
       'path': 'github.com/go-delve/delve/cmd/dlv',
-      'version': '1.23.1',
+      'version': '1.24.2',
     },
     'adapters': {
       "delve": {
